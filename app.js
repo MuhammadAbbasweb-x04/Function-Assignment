70c6a420b717fa8b4524450e326704ab88f4835d
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-07-28 14:23:22 +0500

    added

---
+++ app.js
@@ -0,0 +1,38 @@
+// Q1
+// Answer
+// function sum (sum1, sum2){
+// console.log(sum1 + sum2);
+// return sum1 + sum2;
+// }
+// sum(2,3);
+
+
+// Q2
+// Answer
+// function num (num1, num2){
+// console.log(num1 - num2);
+// return num1 - num2 ;
+// }
+
+// num(50,50)
+
+
+// Q3
+// Answer
+// function value (val1, val2){
+//     console.log(val1 * val2);
+//     return val1 * val2 ;
+// }
+
+// value(10,2);
+
+// Q4
+// Answer
+// function num(val1, val2){
+// console.log( val1 / val2);
+// return val1 / val2;
+
+// }
+
+// num(23, 2);
+
