c52c751f6a938a8deebcc65b228f68d7df2bfa40
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-08-02 14:18:42 +0500

    added

--- app.js
+++ app.js
@@ -36,6 +36,100 @@
 
 // num(23, 2);
 
-// Q4
+// Q5
+// Answer
+// let sum1 = 10;
+// let sum2 = 2;
+// function val(sum1 , sum2){
+//     console.log(sum1 / sum2);   
+//      return sum1 % sum2;
+// }
+// val(1,1);
+
+// Q6
+// Answer
+// function positive(positive){
+// if( positive > 0){
+// return "positive";
+// }
+// else if( positive < 0){
+// return "nagative"
+// }
+// }
+
+//  console.log(positive(-2));
+
+
+// Q7
+// Answer
+// function nagative(nagative){
+// if(nagative < 0){
+// return "Nagative";
+// }
+// else if(nagative > 0){
+// return "Not nagative";
+// }
+// }
+
+// console.log(nagative(2));
+
+// Q8
+// Answer
+// function evenodd(evenodd){
+// if(evenodd %2 === 0){
+// return "Even";
+// } 
+//  else if (evenodd % 2 !== 0 ){
+// return "Odd";
+//  }
+// }
+// console.log(evenodd(13));
+
+// Q9
+// Answer
+// function equal(num1, num2){
+// if(num1 === num2 && num1 === num2){
+// return "Equal";
+// } else {
+//     return "Not equal";
+// }
+// }
+
+// console.log(equal(20,20));
+
+// Q10
+// Answer
+// function num(num1, num2){
+// if(num1 > num2){
+// return "greater"
+// } else{
+//     return "smaller"
+// }
+
+// }
+// console.log(num(0, 1));
+
+// Q11
+// Answer
+// function  fullname(firstname, lastname) {
+//     return firstname + "" + lastname; 
+// }
+// console.log(fullname("muhammad","abbas"));
+
+// Q12
 // Answer
 
+// function score(score){
+// if(score > 40){
+// return "pass";
+// }
+// else{
+//     return "fail";
+// }
+
+// }
+// console.log(score(100));
+
+
+// Q13
+// Answer
